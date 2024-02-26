a21790c2f21ea6d09b6cb9d23b44e905edb95e2e
wzh4464_gpu <32484940+wzh4464@users.noreply.github.com>
2024-02-26 12:05:28 +0800

    triangulize boxbox

--- self_defined_mesh/boxbox.m
+++ self_defined_mesh/boxbox.m
@@ -15,23 +15,44 @@ big_faces = [1 9 10 2; 2 10 11 3; 3 11 12 4; 4 12 9 1; 1 2 3 4; 5 13 14 6; 6 14 
 
 faces = [big_faces; small_faces];
 
-% figure;
+% 假设faces是一个Nx4的矩阵，N是四边形的数量
+numQuads = size(faces, 1);
 
-% % 绘制立方体
-% patch('Vertices',vertices,'Faces',faces,'FaceColor','r', 'FaceAlpha', 0.1);
+% 定义将四边形拆分为两个三角形的索引模式
+% 对于四边形[a, b, c, d]，两个三角形为[a, b, c]和[a, c, d]
+% 此处创建一个矩阵，每个四边形生成两个三角形的顶点索引
+triIndexPattern = [1, 2, 3, 1, 3, 4];
 
-% axis equal; % 保证各轴等比缩放
-% xlabel('X');
-% ylabel('Y');
-% zlabel('Z');
-% view(3); % 设置3D视图
+triFaces = faces(:, triIndexPattern);
 
-% % 在每个顶点位置添加顶点编号
-% for i = 1:size(vertices, 1)
-%     text(vertices(i,1), vertices(i,2), vertices(i,3), num2str(i), ...
-%         'HorizontalAlignment','center', ...
-%         'VerticalAlignment','bottom');
-% end
+triFaces = reshape(triFaces', 3, [])';
+
+faces = triFaces;
+
+draw = true;
+
+if draw
+    figure;
+
+    % 绘制立方体
+    patch('Vertices',vertices,'Faces',faces,'FaceColor','r', 'FaceAlpha', 0.1);
+
+    axis equal; % 保证各轴等比缩放
+    xlabel('X');
+    ylabel('Y');
+    zlabel('Z');
+    view(3); % 设置3D视图
+
+    % 在每个顶点位置添加顶点编号
+    for i = 1:size(vertices, 1)
+        text(vertices(i,1), vertices(i,2), vertices(i,3), num2str(i), ...
+            'HorizontalAlignment','center', ...
+            'VerticalAlignment','bottom');
+    end
+
+    saveas(gcf, 'cube.png');
+
+end
 
 surface.vertices = vertices;
 surface.faces = faces;
